e5cb73ad5b9ff4f295a6b1ceb8a5e26a5e2cba58
Maxim Nechaev <4max.nechaev@gmail.com>
2018-09-13 14:04:43 +0000

    Admin categories added

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,29 @@
+class Admin::CategoriesController < ApplicationController
+
+  def index_category
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new_category
+    @category = Category.new
+  end
+
+  def create_category
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
+
+end
